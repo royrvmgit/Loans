ff1d3e4428796f66ddd1db5e817c598b24385673
royrvmgit <royrvm.oficial@gmail.com>
2023-06-26 20:28:33 -0500

    Begin countries API controller

---
+++ Loans.API/Controllers/CountriesController.cs
@@ -0,0 +1,33 @@
+﻿using Loans.API.Data;
+using Loans.Shared.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Loans.API.Controllers
+{
+    [ApiController]
+    [Route("/api/countries")]
+    public class CountriesController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public CountriesController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAsync() 
+        {
+            return Ok(await _context.Countries.ToListAsync());
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> PostAsync(Country country)
+        {
+            _context.Add(country);
+            await _context.SaveChangesAsync();
+            return Ok(country);
+        }
+    }
+}
